f0cd3cba8ed6e1959ff8069d2ea36ac8d0bc5caa
Junhao Qu <869142649qq@gmail.com>
2024-02-15 15:01:19 -0800

    correct the name

--- app/src/main/java/edu/ucsd/cse110/successorator/app/MainActivity.java
+++ app/src/main/java/edu/ucsd/cse110/successorator/app/MainActivity.java
@@ -17,6 +17,7 @@ public class MainActivity extends AppCompatActivity {
     private GoalListAdapter finishedGoalsAdapter;
     private SimpleGoalRepository goalRepository;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
